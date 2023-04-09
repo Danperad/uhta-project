f5d58bdf6c36c9b90b5f7ca3af79bf6ad77ad5e2
Danperad <red.danperad@gmail.com>
2023-04-09 15:37:51 +0300

    refactor sql script

--- db/setup.sql
+++ db/setup.sql
@@ -7,25 +7,25 @@ GO
 
 CREATE TABLE uhta.materials
 (
-    material_id               int IDENTITY (1, 1),
-    csss                      int         NOT NULL,
-    nr_3                      int         NOT NULL,
-    title                     nvarchar(100) NOT NULL,
-    producer                  nvarchar(100) NOT NULL,
-    material_type             nvarchar(9) NOT NULL CHECK (material_type IN (N'Прибор', N'Расходник')),
-    unit_of_measurement       nvarchar(3) NOT NULL CHECK ( unit_of_measurement IN (N'ШТ', N'КМП', N'М', N'УПК', N'КГ', N'Т', N'М2') ),
-    is_deleted                bit         NOT NULL DEFAULT (0),
-    device_count_in_stock     int         NOT NULL DEFAULT (0),
-    device_count_in_operation int         NOT NULL DEFAULT (0),
+    material_id         int IDENTITY (1, 1),
+    csss                int           NOT NULL,
+    nr_3                int           NOT NULL,
+    title               nvarchar(100) NOT NULL,
+    producer            nvarchar(100) NOT NULL,
+    material_type       nvarchar(9)   NOT NULL CHECK (material_type IN (N'Прибор', N'Расходник')),
+    unit_of_measurement nvarchar(3)   NOT NULL CHECK ( unit_of_measurement IN (N'ШТ', N'КМП', N'М', N'УПК', N'КГ', N'Т', N'М2') ),
+    is_deleted          bit           NOT NULL DEFAULT (0),
+    count_in_stock      int           NOT NULL DEFAULT (0),
+    count_in_operation  int           NOT NULL DEFAULT (0),
     CONSTRAINT pk_device PRIMARY KEY (material_id)
-)
+);
 
 CREATE TABLE uhta.binding
 (
     device_id      int NOT NULL,
     consumables_id int NOT NULL,
     CONSTRAINT pk_binding PRIMARY KEY (device_id, consumables_id)
-)
+);
 
 CREATE TABLE uhta.reports
 (
@@ -34,7 +34,7 @@ CREATE TABLE uhta.reports
     date_from   date          NOT NULL,
     date_before date          NOT NULL,
     CONSTRAINT pk_report PRIMARY KEY (report_id)
-)
+);
 
 CREATE TABLE uhta.applications
 (
@@ -42,17 +42,17 @@ CREATE TABLE uhta.applications
     application_date   date NOT NULL,
     application_period bigint,
     CONSTRAINT pk_application PRIMARY KEY (application_id)
-)
-
+);
 
 CREATE TABLE uhta.materials_application
 (
-    material_id      int NOT NULL,
-    application_id   int NOT NULL,
-    material_count   int NOT NULL,
+    material_id    int NOT NULL,
+    application_id int NOT NULL,
+    material_count int NOT NULL,
+    CONSTRAINT pf_materials_application PRIMARY KEY (material_id, application_id),
     CONSTRAINT fk_device_application_device FOREIGN KEY (material_id) REFERENCES uhta.materials (material_id),
     CONSTRAINT fk_device_application_application FOREIGN KEY (application_id) REFERENCES uhta.applications (application_id)
-)
+);
 
 CREATE TABLE uhta.users
 (
@@ -61,4 +61,4 @@ CREATE TABLE uhta.users
     user_password nvarchar(64) NOT NULL,
     user_role     nvarchar(10) NOT NULL CHECK (user_role IN (N'ADMIN', N'WORKER', N'GUEST')),
     CONSTRAINT pk_user PRIMARY KEY (user_id)
-)
+);
--- webserver/src/main/kotlin/com/vyatsu/lukoilweb/models/Material.kt
+++ webserver/src/main/kotlin/com/vyatsu/lukoilweb/models/Material.kt
@@ -3,11 +3,11 @@ package com.vyatsu.lukoilweb.models
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "devices")
-data class Device(
+@Table(name = "materials")
+data class Material(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "device_id")
+    @Column(name = "material_id")
     val id: Int,
 
     @Column(name = "csss")
@@ -16,15 +16,21 @@ data class Device(
     @Column(name = "nr_3")
     val nr: Int,
 
+    @Column(name = "title")
+    val title: String,
+
+    @Column(name = "producer")
+    val producer: String,
+
     @Column(name = "unit_of_measurement")
     val unitOfMeasurement: String,
 
-    @Column(name = "deleted")
+    @Column(name = "id_deleted")
     val isDeleted: Boolean = false,
 
-    @Column(name = "consumable_count_in_stock")
-    val consumableCountInStock: Int = 0,
+    @Column(name = "count_in_stock")
+    val inStock: Int = 0,
 
-    @Column(name = "consumable_count_in_operation")
-    val consumableCountInOperation: Int = 0
+    @Column(name = "count_in_operation")
+    val inOperation: Int = 0
 )
