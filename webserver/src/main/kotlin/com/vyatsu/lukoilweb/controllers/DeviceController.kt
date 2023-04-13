32b22a14d0ab7c1055e6d30a5ea6c6ebc98325c3
Danperad <red.danperad@gmail.com>
2023-04-13 19:08:52 +0300

    fix cors

--- webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/DeviceController.kt
+++ webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/DeviceController.kt
@@ -2,12 +2,10 @@ package com.vyatsu.lukoilweb.controllers
 
 import com.vyatsu.lukoilweb.models.Device
 import com.vyatsu.lukoilweb.services.DeviceService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @RequestMapping("api/devices")
 class DeviceController(private val deviceService: DeviceService) {
     @GetMapping(produces = ["application/json"])
