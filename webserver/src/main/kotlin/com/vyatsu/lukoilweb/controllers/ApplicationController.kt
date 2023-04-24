a6df3bf0ab4069946721abf04603e50e9382569c
RededPanda <red.danperad@gmail.com>
2023-04-24 19:55:18 +0300

    hothotfix cors

--- webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/ApplicationController.kt
+++ webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/ApplicationController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = ["*"])
 @RequestMapping("api/applications")
 class ApplicationController(private val applicationService: ApplicationService) {
     @GetMapping("/",produces = ["application/json"])
