394ec298746ebdba8461c25c503ac82ddc2911e8
RededPanda <red.danperad@gmail.com>
2023-04-24 19:50:13 +0300

    hotfix cors

--- webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/ConsumableController.kt
+++ webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/ConsumableController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/consumables")
 class ConsumableController(private val consumableService: ConsumableService) {
     @GetMapping("/",produces = ["application/json"])
