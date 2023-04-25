98235f9f1e94ac06edcc06ae5d9c4dfbd32fc973
RededPanda <red.danperad@gmail.com>
2023-04-25 17:29:00 +0300

    add pagination

--- webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/DeviceController.kt
+++ webserver/src/main/kotlin/com/vyatsu/lukoilweb/controllers/DeviceController.kt
@@ -3,6 +3,7 @@ package com.vyatsu.lukoilweb.controllers
 import com.vyatsu.lukoilweb.models.DeviceModel
 import com.vyatsu.lukoilweb.services.DeviceService
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -12,17 +13,20 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("api/devices")
 class DeviceController(private val deviceService: DeviceService) {
     @Cacheable("devices")
-    @GetMapping("/",produces = ["application/json"])
-    fun getAllDevices(): ResponseEntity<Set<DeviceModel>> {
-        val devices = deviceService.findAllDevices()
+    @GetMapping("/", produces = ["application/json"])
+    fun getAllDevices(
+        @RequestParam(required = false) start: Int = 0,
+        @RequestParam(required = false) count: Int = 10
+    ): ResponseEntity<Set<DeviceModel>> {
+        val devices = deviceService.findAllDevicePage(PageRequest.of(start, count))
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(devices)
     }
 
     @Cacheable("device")
     @GetMapping("{id}", produces = ["application/json"])
-    fun getDeviceById(@PathVariable id: Int): ResponseEntity<DeviceModel?>{
+    fun getDeviceById(@PathVariable id: Int): ResponseEntity<DeviceModel?> {
         val device = deviceService.findDeviceById(id)
-        return if (device != null){
+        return if (device != null) {
             ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(device)
         } else {
             ResponseEntity.notFound().build()
--- webserver/src/main/kotlin/com/vyatsu/lukoilweb/repositories/DeviceRepository.kt
+++ webserver/src/main/kotlin/com/vyatsu/lukoilweb/repositories/DeviceRepository.kt
@@ -1,11 +1,11 @@
 package com.vyatsu.lukoilweb.repositories
 
 import com.vyatsu.lukoilweb.models.Device
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface DeviceRepository : CrudRepository<Device, Int> {
+interface DeviceRepository : JpaRepository<Device, Int> {
     fun findDeviceByNr(nr: Int) : Device?
     fun findDeviceById(id: Int) : Device?
 
